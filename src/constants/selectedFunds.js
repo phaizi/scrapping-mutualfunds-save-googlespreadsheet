d0df12d7867eb91cff2fc2eca97404bb0b50e7e9
phaizi <faizan.mansur87@gmail.com>
2024-03-15 04:11:51 +0500

    initialized the project with all files and dependencies

---
+++ src/constants/selectedFunds.js
@@ -0,0 +1,54 @@
+const SELECTED_FUNDS = [
+  {
+    ShariahCompliantMoneyMarket: [
+      'Meezan Cash Fund',
+      'Meezan Rozana Amdani Fund',
+    ],
+    'ShariahCompliantFixedRate/Return': [
+      'Meezan Paaidaar Munafa Plan IV',
+      'Meezan Paaidaar Munafa Plan V',
+      'Meezan Paaidaar Munafa Plan VI',
+      'Meezan Paaidaar Munafa Plan VII',
+      'Meezan Paaidaar Munafa Plan VIII',
+    ],
+    ShariahCompliantIncome: [
+      'Meezan Daily Income Fund (MDIP I)',
+      'Meezan Daily Income Fund (Meezan Mahana Munafa Plan)',
+      'Meezan Daily Income Fund (Meezan Munafa Plan I)',
+      'Meezan Daily Income Fund (Meezan Sehl Account Plan) (MSHP)',
+      'Meezan Islamic Income Fund',
+      'Meezan Sovereign Fund',
+    ],
+  },
+  {
+    ShariahCompliantFundofFundsCPPI: [
+      'Meezan Strategic Allocation Fund II (Meezan Capital Preservation Plan IV)',
+      'Meezan Strategic Allocation Fund II (Meezan Capital Preservation Plan VI)',
+      'Meezan Strategic Allocation Fund II (Meezan Capital Preservation Plan VII)',
+      'Meezan Strategic Allocation Fund III (Meezan Capital Preservation Plan IX)',
+    ],
+    ShariahCompliantBalancedFund: ['Meezan Balanced Fund'],
+    ShariahCompliantAssetAllocation: ['Meezan Asset Allocation Fund'],
+    ShariahCompliantFundofFunds: [
+      'Meezan Financial Planning Fund of Funds (Aggressive)',
+      'Meezan Financial Planning Fund of Funds (Conservative)',
+      'Meezan Financial Planning Fund of Funds (MAAP I)',
+      'Meezan Financial Planning Fund of Funds (Moderate)',
+      'Meezan Financial Planning Fund of Funds (Very Conservative Allocation Plan)',
+      'Meezan Strategic Allocation Fund (MSAP I)',
+      'Meezan Strategic Allocation Fund (MSAP II)',
+      'Meezan Strategic Allocation Fund (MSAP III)',
+      'Meezan Strategic Allocation Fund (MSAP IV)',
+      'Meezan Strategic Allocation Fund (MSAP V)',
+    ],
+    ShariahCompliantCommodities: ['Meezan Gold Fund'],
+    ShariahCompliantIndexTracker: ['KSE Meezan Index Fund'],
+    ShariahCompliantEquity: [
+      'Al Meezan Mutual Fund',
+      'Meezan Energy Fund',
+      'Meezan Islamic Fund',
+    ],
+  },
+]
+
+export default SELECTED_FUNDS
